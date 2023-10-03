0c38d030f0c38c59ac7c70ff97bf4cef494e9704
Kyrylo Dyachenko <104459315+kirill25460@users.noreply.github.com>
2023-10-03 21:35:33 +0300

    try main section

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,14 +1,34 @@
-import { MainContainer,  MainText, MainPic } from './MainSection.styled';
+import {
+  MainContainer,
+  MainText,
+  MainLeft,
+  MainButton,
+  MainPic,
+  ParallaxPic,
+  MainPar,
+} from './MainSection.styled';
 import man from 'images/wizard.png';
-
+import detail from "images/3d-detail-gear.png";
+import { MouseParallax } from 'react-just-parallax';
 
 const MainSection = () => {
   return (
     <MainContainer>
-
-      <MainText>Книга, це книга, люби кого? Книгу.</MainText>
-    
-    <MainPic src={man} alt="Man"/>
+       <MouseParallax>
+      <ParallaxPic src={detail} alt="detail"/>
+    </MouseParallax>
+      <MainLeft>
+        <MainText>Книга, це книга, люби кого? Книгу.</MainText>
+        <MouseParallax>
+      <ParallaxPic src={detail} alt="detail"/>
+    </MouseParallax>
+        <MainPar>Книга, книга книга книга, книжки? Книга! Книга книга, книга. Книга, книга книга книга, книжки? Книга! </MainPar>
+        <MainButton>Галерея наших робіт</MainButton>
+      </MainLeft>
+      <MouseParallax>
+      <ParallaxPic src={detail} alt="detail"/>
+    </MouseParallax>
+      <MainPic src={man} alt="Man" />
     </MainContainer>
   );
 };
--- src/components/MainSection/MainSection.styled.jsx
+++ src/components/MainSection/MainSection.styled.jsx
@@ -1,29 +1,71 @@
 import styled from 'styled-components';
-import GearsImg from '../../images/gears2.png'
+import GearsImg from '../../images/gears2.png';
 
-export const MainContainer = styled.div`
-    background-image: url(${GearsImg});
-    background-size: cover; /* Изображение будет масштабировано так, чтобы покрыть всю область фона */
-    background-repeat: no-repeat; /* Запрет повторения изображения */
-     background-attachment: fixed; /*Фиксация изображения, чтобы оно не двигалось при прокрутке */
-    background-position: center center;
-    width: auto;
-    height: 730px;
-    display:flex;
+export const MainContainer = styled.div`overflow:hidden;
+  background-image: url(${GearsImg});
+  background-size: cover; /* Изображение будет масштабировано так, чтобы покрыть всю область фона */
+  background-repeat: no-repeat; /* Запрет повторения изображения */
+  background-attachment: fixed; /*Фиксация изображения, чтобы оно не двигалось при прокрутке */
+  background-position: center center;
+  width: auto;
+  height: 730px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
-export const MainText = styled.p`
+export const MainLeft = styled.div`
+margin-right:300px;
+
+`;
+
+export const MainText = styled.h2`
   font-family: 'Raleway';
   font-style: normal;
   font-weight: 700;
   font-size: 56px;
   line-height: 66px;
   margin: 0;
-
+width:574px;
   color: #ffffff;
 `;
 
-export const MainPic = styled.img`
-width:880px;
-height:773px;
+export const MainPar = styled.p`
+margin: 0;
+width:574px;
+padding-top:25px;
+padding-bottom:70px;
+color: #AF9A91;
+font-family: DM Sans;
+font-size: 18px;
+font-style: normal;
+font-weight: 400;
+line-height: 22px; /* 122.222% */
 `;
+
+
+export const MainButton = styled.button`
+display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 255px;
+  height: 66px;
+  border-radius: 40px;
+  border: 0;
+  background-color: #fff;
+  color: #5c3d2e;
+  font-family: 'Raleway';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 18px;
+
+`;
+
+export const MainPic = styled.img`
+  width: 755px;
+  height: 731px;
+`;
+
+
+export const ParallaxPic = styled.img`position:absolute;`;
