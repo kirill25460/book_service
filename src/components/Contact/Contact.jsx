9c9fdd70b7e1699d8dced411f823f769ef86379a
Vlad Melnik <vmelniki2828@gmail.com>
2023-10-20 20:01:01 +0300

    fix text

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -99,7 +99,7 @@ const Contact = () => {
         </FormContainer>
       </Left>
       <Right>
-        <RightSmallText>Зв`язок з нами</RightSmallText>
+        <RightSmallText>Зв'язок з нами</RightSmallText>
         <RightMainTitle>Завжди раді вас чути!</RightMainTitle>
         <RightSubTitle>
           Залиште свою заявку у формі зворотнього зв&rsquo;язку і ми зв&rsquo;яжемось з вами
